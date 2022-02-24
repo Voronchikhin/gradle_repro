88c8dba84204747ff52d9a08b6236db83b3fbcdd
Maxim Voronchikhin <maxim.voronchikhin@jetbrains.com>
2022-02-24 19:16:21 +0700

    add some dependencies

--- kts/build.gradle.kts
+++ kts/build.gradle.kts
@@ -12,8 +12,10 @@ repositories {
 dependencies {
     // https://mvnrepository.com/artifact/com.h2database/h2
     implementation("com.h2database:h2:1.4.199")
+    implementation("junit:junit:4.12")
+    implementation("io.ktor:ktor-client-apache:1.6.7")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
